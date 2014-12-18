31c856eaa467e7ff1da2416f9e7200744c33e7b5
Piotr S. Staszewski <p.staszewski@gmail.com>
2014-12-18 17:57:14 +0100

    1/1: add more test cases

--- set_1_test.go
+++ set_1_test.go
@@ -11,6 +11,9 @@ type caseHexToBase64 struct {
 func TestHexToBase64(t *testing.T) {
 	cases := []caseHexToBase64{
 		caseHexToBase64{"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d", "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"},
+		caseHexToBase64{"1234", "EjQ="},
+		caseHexToBase64{"ffff", "//8="},
+		caseHexToBase64{"deadbeef", "3q2+7w=="},
 	}
 
 	for i, c := range cases {
